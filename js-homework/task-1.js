5d3f9f80d78d21722e4bf09aa61211ca39974ced
Viktor Vashchenko <viktor2236@gmail.com>
2020-12-20 20:27:24 +0200

    little update

--- js-homework/task-1.js
+++ js-homework/task-1.js
@@ -2,6 +2,6 @@ const listItems = categories.querySelectorAll('li.item');
 
 
 for (let item of listItems) {
-    console.log(`Категория: ${item.firstElementChild.textContent} \nКоличество элементов: ${item.querySelectorAll('li').length}`)
+    console.log(`Категория: ${item.firstElementChild.textContent} \nКоличество элементов: ${item.lastElementChild.childElementCount}`) 
 }
 
